d4cfc9f84c724f491623f9bf59a5806759fefeee
Arpit Bhardwaj <arpit.bhardwaj@sap.com>
2020-01-12 11:13:47 +0530

    State Pattern Demo

---
+++ src/main/java/com/ab/state/State.java
@@ -0,0 +1,9 @@
+package com.ab.state;
+
+public abstract class State {
+
+    public abstract void handleRequest();
+    /*public void handleRequest(){
+        System.out.println("Shoudn't be able to get here");
+    }*/
+}
---
+++ src/main/java/com/ab/state/StateDemo.java
@@ -0,0 +1,21 @@
+package com.ab.state;
+
+public class StateDemo {
+    public static void main(String[] args) {
+        Fan fan = new Fan();
+
+        System.out.println(fan);
+
+        fan.pullChain();
+
+        System.out.println(fan);
+
+        fan.pullChain();
+
+        System.out.println(fan);
+
+        fan.pullChain();
+
+        System.out.println(fan);
+    }
+}
